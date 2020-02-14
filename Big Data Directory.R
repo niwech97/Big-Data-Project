45e2587628bddca53a1959d2586275bd6daf8b5e
amtalman97 <60628674+amtalman97@users.noreply.github.com>
2020-02-14 15:34:34 -0800

    test2

--- Big Data Directory.R
+++ Big Data Directory.R
@@ -10,13 +10,13 @@ R.version.string
 working.dir <- getwd()
 
 #store future file names in an object... These are base folder names
-output.folder.names <- c("Clean Folder", "Figures of interest", 
+output.folder.names <- c("Clean Folder", "Figures of interest",
                          "Raw Data","Final R scripts","Potential Analysis",
                          "Relevant Analysis","Relevant Graphics","Useful R scripts")
 
-# and make the folders if they don't exit yet. 
-for(i in 1:length(output.folder.names)) 
-  if(file.exists(output.folder.names[i]) == FALSE) 
+# and make the folders if they don't exit yet.
+for(i in 1:length(output.folder.names))
+  if(file.exists(output.folder.names[i]) == FALSE)
     dir.create(output.folder.names[i])
 #these are the pathways which are necessary to send graphs to the folders.
 path.clean <- paste(working.dir, "/", output.folder.names[1], "/", sep = "")
@@ -28,6 +28,15 @@ path.finan<- paste(working.dir, "/", output.folder.names[6], "/", sep = "")
 path.fingraph<- paste(working.dir, "/", output.folder.names[7], "/", sep = "")
 path.extr<- paste(working.dir, "/", output.folder.names[8], "/", sep = "")
 
+The things you wrote don't show up on my version, i think its because my teletype was off when you were typing it
+can you try pushing these changes to the repository on github and ill try to accept them?
 
-
-
+#dodgy notes from Thors office hours. Loop for dividing data frame by country
+# combine <-unique(d$country)
+# storage.r <- rep(length(country))
+# names( )<-
+# for(i 1:length(country)){
+# print(i)
+# dt<-d[d$country==row[i],]
+# storage.r<-linearmodel(...)
+#}
