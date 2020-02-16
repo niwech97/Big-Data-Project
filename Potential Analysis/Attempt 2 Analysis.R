9a3563d5482ba10f071b315e28bfce8a6551e5b7
niwech97 <niwech97@gmail.com>
2020-02-16 14:43:54 -0800

    Created and cleaned new datasets
    Co-Authored-By: amtalman97 <amtalman97@users.noreply.github.com>
    Co-Authored-By: slmorassutti <60947122+slmorassutti@users.noreply.github.com>

--- Potential Analysis/Attempt 2 Analysis.R
+++ Potential Analysis/Attempt 2 Analysis.R
@@ -1,4 +1,19 @@
 # Attempt 2 potan GDP SCC vacc
 
+#Created mcr15 object through file screen in R, with our own factor list as countries 
+# doing the same for GDP data
 
+
+
+
+
+
+
+length(mcr15$LOCATION)   #these dont need to be the same as our defined factors are the same
+length(GDP15$LOCATION)   #therefore we can just remove our NA factors when making the linear model
+
+GDP15<-select(GDP15, -c("SUBJECT", "INDICATOR", "MEASURE", "Flag Codes", 
+                      "FREQUENCY"))
+mcr15<-select(mcr15, -c("SUBJECT", "INDICATOR", "MEASURE", "Flag Codes", 
+                      "FREQUENCY"))
 
---
+++ Useful R scripts/Notes.R
@@ -0,0 +1,10 @@
+#Notes
+
+
+
+Warning: 4 parsing failures.
+row      col           expected actual                                      file
+115 LOCATION value in level set    BRA 'Raw Data/Measles.Child.VacRate15-18.csv'
+116 LOCATION value in level set    BRA 'Raw Data/Measles.Child.VacRate15-18.csv'
+117 LOCATION value in level set    BRA 'Raw Data/Measles.Child.VacRate15-18.csv'
+118 LOCATION value in level set    BRA 'Raw Data/Measles.Child.VacRate15-18.csv'
